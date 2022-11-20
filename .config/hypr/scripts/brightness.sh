b24b547b32c59b826a2799d45c11acb94b0a2e21
InioX <justimnix@gmail.com>
2022-11-20 13:31:50 +0100

    fix script

--- .config/hypr/scripts/brightness.sh
+++ .config/hypr/scripts/brightness.sh
@@ -3,50 +3,65 @@
 #Usage: ./brightness.sh [temperature-up / temperature-down / temperature-mute] [int]
 
 dir=`dirname "$0"`
+icon_folder="~/.config/hypr/icons"
 amount=$2
 id_brightness="8054"
 id_temperature="8055"
+value_brightness=`timeout 0.01 wl-gammarelay --subscribe Brightness &`
+value_temperature=`timeout 0.01 wl-gammarelay --subscribe Temperature &`
 
 # Functions
-function value_temperature {
-    timeout 0.01 wl-gammarelay --subscribe Temperature &
-}
-
-function value_brightness {
-    timeout 0.01 wl-gammarelay --subscribe Brightness &
+function get_value {
+    local percentage=`expr $1*100/1 | bc`
+    if [[ "$percentage" -eq 100 ]]; then
+        value=100 
+    elif [[ "$percentage" -eq 0 ]]; then
+        value=0
+    else
+        value=$percentage
+    fi
 }
 
 function notification_bar {
-    dunstify -u low -r "$id" -h int:value:"$value" ${text}
+    dunstify -u low -r "$id" -h int:value:"$value" "${text}" -a "${header}" -i "${icon}"
 }
 
 case $1 in
 # Brightness
 brightness-up)
-    value=`expr $(value_brightness)*100/1 | bc`
-    text="Brightness: ${value}%"
+    get_value $value_brightness
+    # value=$(get_percentage $value_brightness)
+    header="Brightness"
+    text="Currently at ${value}%"
+    icon="$icon_folder/brightness-up.png"
     id=$id_brightness
 	busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateBrightness d +${amount}
 	notification_bar;;
 
 brightness-down)
-    value=`expr $(value_brightness)*100/1 | bc`
-    text="Brightness: ${value}%"
+    get_value $value_brightness
+    header="Brightness"
+    text="Currently at ${value}%"
+    icon="$icon_folder/brightness-down.png"
     id=$id_brightness
 	busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateBrightness d -${amount}
 	notification_bar;;
 
 # Temperature
 temperature-up)
-    value=$(value_temperature)
-    text="Temperature: ${value}K"
+    value=$(($value_temperature + $amount))
+    header="Temperature"
+    text="Currently at ${value}K"
+    icon="$icon_folder/temperature-up.png"
     id=$id_temperature
     busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n +${amount}
 	notification_bar;;
 
 temperature-down)
-    value=$(value_temperature)
-    text="Temperature: ${value}K"
+    value=$(($value_temperature - $amount))
+    header="Temperature"
+    text="Currently at ${value}K"
+    icon="$icon_folder/temperature-down.png"
     id=$id_temperature
     busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n -${amount}
 	notification_bar;;
--- .config/hypr/scripts/volume.sh
+++ .config/hypr/scripts/volume.sh
@@ -3,19 +3,19 @@
 #Usage: ./volume.sh [input-up / input-down / input-mute] [int]
 
 dir=`dirname "$0"`
+icon_folder="~/.config/hypr/icons"
 amount=$2
 id_output="8052"
 id_input="8053"
+timeout=2000
+volume_input=$(amixer -D pulse get Capture | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1)
+volume_output=$(amixer -D pulse get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1)
+
+function get_volume {
+    if [[ "$1" -eq 100 ]]; then $1=100; else $1=$(($1 + $amount)); fi
+}
 
 # Functions
-function volume_input {
-    amixer -D pulse get Capture | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
-}
-
-function volume_output {
-    amixer -D pulse get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
-}
-
 function is_muted_input {
     amixer -D pulse get Capture | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
 }
@@ -25,64 +25,84 @@ function is_muted_output {
 }
 
 function notification_bar {
-    dunstify -u low -r "$id" -h int:value:"$volume" ${text}
+    notify-send -u low -r "$id" -a "${header}" -i "${icon}" -h int:value:"$volume" "${text}" -t $timeout
 }
 
 function notification_mute {
-    notify-send "$text" -u low -r $id
+    notify-send "$text" -u low -r $id -a "$header" -i "${icon}" -t $timeout
 }
 
 case $1 in
 # Output
 output-up)
-    volume=$(volume_output)
-    text="Volume: ${volume}%"
+    get_volume $volume_output
+    volume=$volume_output
+    header="Output volume"
+    text="Currently at ${volume}%"
+    icon="$icon_folder/vol-up.png"
     id=$id_output
 	amixer -D pulse sset Master "$amount"%+
 	notification_bar;;
 
 output-down)
-    volume=$(volume_output)
-    text="Volume: ${volume}%"
+    volume=$(($volume_output - $amount))
+    header="Output volume"
+    text="Currently at ${volume}%"
+    icon="$icon_folder/vol-down.png"
     id=$id_output
 	amixer -D pulse sset Master "$amount"%-
 	notification_bar;;
 
 output-mute)
-    id=$id_output
     if is_muted_output; then
-        text="Volume unmuted"
+        header="Output"
+        text="Unmuted."
+        icon="$icon_folder/vol.png"
+        id=$id_output
         amixer -D pulse set Master 1+ toggle
         notification_mute
     else
-        text="Volume muted"
+        header="Output"
+        text="Muted."
+        icon="$icon_folder/vol-muted.png"
+        id=$id_output
         amixer -D pulse set Master 1+ toggle
         notification_mute
     fi;;
 
 # Input
 input-up)
-    volume=$(volume_input)
-    text="Microphone: $volume%"
+    get_volume $volume_input
+    volume=$volume_input
+    header="Input volume"
+    text="Currently at ${volume}%"
+    icon="$icon_folder/mic-up.png"
     id=$id_input
 	amixer -D pulse sset Capture "$amount"%+
 	notification_bar;;
 
 input-down)
-    volume=$(volume_input)
-    text="Microphone: $volume%"
+    volume=$(($volume_input - $amount))
+    header="Input volume"
+    text="Currently at ${volume}%"
+    icon="$icon_folder/mic-down.png"
     id=$id_input
 	amixer -D pulse sset Capture "$amount"%-
 	notification_bar;;
 
 input-mute)
-    id=$id_input
     if is_muted_input; then
-        text="Microphone unmuted"
+        header="Microphone"
+        text="Unmuted."
+        icon="$icon_folder/mic.png"
+        id=$id_input
         amixer -D pulse set Capture 1+ toggle
         notification_mute
     else
-        text="Microphone muted"
+        header="Microphone"
+        text="Muted."
+        icon="$icon_folder/mic-muted.png"
+        id=$id_input
         amixer -D pulse set Capture 1+ toggle
         notification_mute
     fi;;
