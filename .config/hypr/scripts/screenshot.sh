f8cad3ec737b7606794587080e0bcb7e7378fdc4
InioX <justimnix@gmail.com>
2022-11-23 23:30:55 +0100

    add icons

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/bash
+timeout=4000
+icon_folder="~/.config/hypr/icons"
+
+function notification {
+    notify-send "$text" -u low -a "$header" -i "${icon}" -t $timeout
+}
+
 case $1 in
   copy)
-    grim - | wl-copy;;
+    icon="${icon_folder}/copy.png"
+    header="Screenshot"
+    text="Copied screen"
+    grim - | wl-copy
+    notification;;
   copy_region)
-    grim -g "$(slurp)" - | wl-copy;;
+    icon="${icon_folder}/copy.png"
+    header="Screenshot"
+    text="Copied region"
+    grim -g "$(slurp)" - | wl-copy
+    notification;;
   save)
+    icon="${icon_folder}/save.png"
+    header="Screenshot"
+    text="Saved screen"
     grim "$(xdg-user-dir PICTURES)/Screenshots/$(date)"
+    notification;;
 esac
