907dadaf37fd07ff40d33f7c43c81994800eabe8
InioX <justimni@gmail.com>
2022-11-27 18:57:55 +0100

    add powermenu and lockscreen

---
+++ .config/hypr/scripts/lockscreen.sh
@@ -0,0 +1,22 @@
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--ring-color 362f2b \
+	--key-hl-color ffbf9b \
+	--text-color ffbf9b \
+	--text-ver-color ffbf9b \
+	--line-color 00000000 \
+	--inside-color 221e1b \
+	--inside-ver-color 221e1b \
+	--inside-wrong-color 221e1b \
+	--ring-ver-color 362f2b \
+	--ring-wrong-color 362f2b \
+	--separator-color 00000000 \
+	--grace 2 \
+	--fade-in 0.2
+
---
+++ .config/hypr/scripts/powermenu.sh
@@ -0,0 +1,38 @@
+#!/bin/env bash
+ 
+# Options for powermenu
+lock=" Lock"
+logout=" Logout"
+shutdown=" Shutdown"
+reboot=" Restart"
+sleep=" Sleep"
+ 
+# Get answer from user via rofi
+selected_option=$(echo "$lock
+$logout
+$sleep
+$reboot
+$shutdown" | rofi -dmenu\
+                  -i\
+                  -p "Power"\
+		  -theme "~/.config/hypr/rofi/powermenu.rasi")
+# Do something based on selected option
+if [ "$selected_option" == "$lock" ]
+then
+    swaylock
+elif [ "$selected_option" == "$logout" ]
+then
+    loginctl terminate-user `whoami`
+elif [ "$selected_option" == "$shutdown" ]
+then
+    sudo systemctl poweroff
+elif [ "$selected_option" == "$reboot" ]
+then
+    sudo systemctl reboot
+elif [ "$selected_option" == "$sleep" ]
+then
+    amixer set Master mute
+    sudo systemctl suspend
+else
+    echo "No match"
+fi
