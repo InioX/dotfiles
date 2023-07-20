614869eeef493a30069bd6806fb301a422640b75
InioX <justimnix@gmail.com>
2023-07-20 16:00:11 +0200

    feat: add swap

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -27,7 +27,12 @@
     fsType = "vfat";
   };
 
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 4 * 1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
