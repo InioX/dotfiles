7b337ea478b64bf959ea4e3c2cbad458c1d79cf4
InioX <justimnix@gmail.com>
2023-08-05 23:01:00 +0200

    fix(reload firefox): check if firefox is running before killing it

--- dotfiles/config/hypr/scripts/reload_firefox.sh
+++ dotfiles/config/hypr/scripts/reload_firefox.sh
@@ -2,5 +2,8 @@
 
 position=$(hyprctl clients -j | jq 'map(select(.class=="firefox"))[].workspace.id')
 
-pkill firefox
-hyprctl dispatch exec [workspace $position silent] firefox > /dev/null 2>&1
+if pgrep -x firefox > /dev/null
+then
+    pkill firefox
+    hyprctl dispatch exec [workspace $position silent] firefox > /dev/null 2>&1
+fi
