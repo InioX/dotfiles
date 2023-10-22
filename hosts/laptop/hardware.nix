eee5fffafa646f63fc3348fca58adcf8e9461905
InioX <justimnix@gmail.com>
2023-10-22 13:04:36 +0200

    style: format with `alejandra`

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -27,7 +27,7 @@
     fsType = "vfat";
   };
 
-  swapDevices = [ ];
+  swapDevices = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
