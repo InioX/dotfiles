3aa11af5e474e4e77f6baeadb7b3dee8ba35acb1
InioX <justimnix@gmail.com>
2023-12-02 00:37:39 +0100

    feat(nix): add hyprland cachix

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -26,10 +26,12 @@ in {
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         ];
         substituters = [
           "https://cache.nixos.org"
           "https://nixpkgs-wayland.cachix.org"
+          "https://hyprland.cachix.org"
         ];
       };
       optimise.automatic = true;
