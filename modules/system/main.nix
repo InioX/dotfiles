c952f5dc0ecf0547a326a125f68ff4768993e88f
InioX <justimnix@gmail.com>
2023-12-30 23:44:13 +0100

    refactor: move `generate-wallpaper` into system module

--- modules/system/main.nix
+++ modules/system/main.nix
@@ -18,6 +18,24 @@ with lib.zenyte; let
       sudo nixos-rebuild switch --flake .# --log-format internal-json |& nom --json
     fi;
   '';
+  fetch-wallpaper = pkgs.writeShellScriptBin "wallfetch" ''
+    if [ ! -f flake.nix ]; then echo "This script is supposed to be ran from flake root." && exit 1; fi;
+
+    path="hosts/$(hostname)/wallpaper.nix"
+
+    sha256=$(curl $1 | sha256sum | cut -d ' ' -f 1 )
+
+    if [ ! -f $path ]; then
+        touch $path
+    fi
+
+    echo $sha256
+
+    echo "{
+      url = \"$1\";
+      sha256 = \"$sha256\";
+    }" > $path
+  '';
 in {
   options.zenyte.system = {
     diffScript = mkBoolOpt true "Enables showing what packages changes between generations on rebuild.";
@@ -65,6 +83,7 @@ in {
         inputs.matugen.packages.${system}.default
 
         rebuild-script
+        fetch-wallpaper
       ];
 
       sessionVariables = {
