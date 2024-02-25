441db6a0bc9b8e093ce3ce5e7bea73b2846aacea
InioX <justimnix@gmail.com>
2024-02-25 17:07:15 +0100

    feat(laptop): change wallpaper

--- hosts/laptop/wallpaper.nix
+++ hosts/laptop/wallpaper.nix
@@ -1,4 +1,4 @@
 {
-  url = "https://cdna.artstation.com/p/assets/images/images/028/339/374/large/aze-k-.jpg";
-  sha256 = "ca1505065071690dfc961c58eda17d8ce0fc6dbec77945d720cb6c57f2c0e387";
+  url = "https://i.pinimg.com/originals/dd/bf/7d/ddbf7d3fc1c6eae2cfb5d2b606c52b28.jpg";
+  sha256 = "0gf9q653d8rm9s3j7vwzf3q9iqnbv77vf7szqp8ppf7yxxsvpjfx";
 }
