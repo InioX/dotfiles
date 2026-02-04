1f1f5d991cc21ea29189a8a07b01ce97d3db86cf
InioX <justimnix@gmail.com>
2026-02-04 20:29:08 +0100

    feat: add discord krisp-patcher and settings.json

--- nixos/modules/apps/discord/default.nix
+++ nixos/modules/apps/discord/default.nix
@@ -8,6 +8,29 @@
 with lib;
 with lib.zenyte; let
   cfg = config.zenyte.apps.discord;
+  # https://github.com/FlameFlag/nixcord/issues/55#issuecomment-2480617637
+  # krisp-patcher $(find ~/.config/discord/ -name "discord_krisp.node" -path "*/modules/discord_krisp/*")
+  krisp-patcher =
+    pkgs.writers.writePython3Bin "krisp-patcher"
+    {
+      libraries = with pkgs.python3Packages; [
+        capstone
+        pyelftools
+      ];
+      flakeIgnore = [
+        "E501" # line too long (82 > 79 characters)
+        "F403" # 'from module import *' used; unable to detect undefined names
+        "F405" # name may be undefined, or defined from star imports: module
+      ];
+    }
+    (
+      builtins.readFile (
+        pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/sersorrel/sys/afc85e6b249e5cd86a7bcf001b544019091b928c/hm/discord/krisp-patcher.py";
+          sha256 = "sha256-h8Jjd9ZQBjtO3xbnYuxUsDctGEMFUB5hzR/QOQ71j/E=";
+        }
+      )
+    );
 in {
   options.zenyte.apps.discord = {
     enable = mkBoolOpt false "Whether to enable discord.";
@@ -22,8 +45,10 @@ in {
     environment.systemPackages = with pkgs; [
       (pkgs.discord.override {
         withOpenASAR = true;
-        withVencord = false;
+        withVencord = true;
       })
+
+      krisp-patcher
     ];
 
     # zenyte.home.configFile."discord/settings.json".text = ''
