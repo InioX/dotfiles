ab7417728349c4f546f72fdc1d4a0a432441b27a
InioX <justimnix@gmail.com>
2026-02-05 15:41:32 +0100

    feat: improve mic quality

--- nixos/modules/system/sound.nix
+++ nixos/modules/system/sound.nix
@@ -17,11 +17,22 @@ in {
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
 
+    environment.systemPackages = with pkgs; [
+      alsa-utils
+      easyeffects
+      helvum
+      pavucontrol
+    ];
+
     services.pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      extraConfig.pipewire."context.properties" = {
+        default.clock.rate = 48000;
+        default.clock.allowed-rates = [44100 48000];
+      };
     };
   };
 }
