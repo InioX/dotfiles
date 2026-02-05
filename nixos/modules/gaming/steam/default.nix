557dc5662c5957ded9b98b02800da08c477609b4
InioX <justimnix@gmail.com>
2026-02-05 21:10:16 +0100

    feat: fix steam and reshade for dbd

--- nixos/modules/gaming/steam/default.nix
+++ nixos/modules/gaming/steam/default.nix
@@ -14,6 +14,14 @@ in {
   };
 
   config = mkIf cfg.enable {
+    # How to enable Reshade for DBD
+    # Follow https://www.reddit.com/r/deadbydaylight/comments/1i9ghol/tutorial_installing_reshade_for_dbd_on_linux/
+    # Do NOT rename d3d11.dll to anything other than dxgi.dll
+    # Update d3dcompiler_47 for Dead by Daylight to work with reshade:
+    # protontricks 381210 d3dcompiler_47
+    # You also need to set Steam Launch Options for DBD to:
+    # WINEDLLOVERRIDES="d3d12=n,b;d3dcompiler_47=n" %command%
+
     environment.systemPackages = with pkgs; [
       mangohud
     ];
@@ -22,6 +30,11 @@ in {
 
     programs.steam = {
       enable = true;
+      protontricks.enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+        # protontricks
+      ];
     };
   };
 }
