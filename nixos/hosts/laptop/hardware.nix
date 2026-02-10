86a5961e5a7924a954793712d2849101fd315555
InioX <justimnix@gmail.com>
2026-02-10 09:01:10 +0100

    feat: change nvidia kernel mode to open, use latest nvidia driver instead of stable

--- nixos/hosts/laptop/hardware.nix
+++ nixos/hosts/laptop/hardware.nix
@@ -35,14 +35,14 @@
     # supported GPUs is at:
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   boot.loader.systemd-boot = {
