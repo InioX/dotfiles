0dc758579decdc2e6c7643f4e7381fb902342e55
InioX <justimnix@gmail.com>
2026-04-02 18:37:25 +0200

    feat: tweak sound settings

--- nixos/modules/system/sound.nix
+++ nixos/modules/system/sound.nix
@@ -25,15 +25,23 @@ in {
       pwvucontrol
     ];
 
+    boot.extraModprobeConfig = ''
+      options snd_hda_intel power_save=0
+      options snd_acp3x_pdm_dma power_save=0
+    '';
+
     services.pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      extraConfig.pipewire."context.properties" = {
-        default.clock.rate = 48000;
-        default.clock.allowed-rates = [44100 48000];
-      };
+      # extraConfig.pipewire."context.properties" = {
+      #   default.clock.rate = 48000;
+      #   default.clock.allowed-rates = [44100 48000 96000];
+      #   default.clock.quantum = 1024;
+      #   default.clock.min-quantum = 512;
+      #   default.clock.max-quantum = 2048;
+      # };
     };
   };
 }
