d0d49fd5320e346e42101fce1500706f310341f6
InioX <justimnix@gmail.com>
2026-06-06 20:17:03 +0200

    refactor: remove old krisp-patcher that doesnt work

--- nixos/modules/apps/discord/discord.nix
+++ nixos/modules/apps/discord/discord.nix
@@ -8,29 +8,6 @@
 with lib;
 with lib.zenyte; let
   cfg = config.zenyte.apps.discord;
-  # https://github.com/FlameFlag/nixcord/issues/55#issuecomment-2480617637
-  # krisp-patcher $(find ~/.config/discord/ -name "discord_krisp.node" -path "*/modules/discord_krisp/*")
-  krisp-patcher =
-    pkgs.writers.writePython3Bin "krisp-patcher"
-    {
-      libraries = with pkgs.python3Packages; [
-        capstone
-        pyelftools
-      ];
-      flakeIgnore = [
-        "E501" # line too long (82 > 79 characters)
-        "F403" # 'from module import *' used; unable to detect undefined names
-        "F405" # name may be undefined, or defined from star imports: module
-      ];
-    }
-    (
-      builtins.readFile (
-        pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/sersorrel/sys/afc85e6b249e5cd86a7bcf001b544019091b928c/hm/discord/krisp-patcher.py";
-          sha256 = "sha256-h8Jjd9ZQBjtO3xbnYuxUsDctGEMFUB5hzR/QOQ71j/E=";
-        }
-      )
-    );
 in {
   options.zenyte.apps.discord = {
     enable = mkBoolOpt false "Whether to enable discord.";
@@ -45,7 +22,6 @@ in {
 
       vesktop
 
-      krisp-patcher
       qtscrcpy
     ];
 
