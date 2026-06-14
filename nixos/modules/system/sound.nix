61992677e1b36eb674155105c966f032c1460a2a
InioX <justimnix@gmail.com>
2026-06-14 19:29:44 +0200

    feat: add back the pipewire sound properties

--- nixos/modules/system/sound.nix
+++ nixos/modules/system/sound.nix
@@ -141,13 +141,13 @@ in
           #   };
         }
       ];
-      # extraConfig.pipewire."context.properties" = {
-      #   default.clock.rate = 48000;
-      #   default.clock.allowed-rates = [44100 48000 96000];
-      #   default.clock.quantum = 1024;
-      #   default.clock.min-quantum = 512;
-      #   default.clock.max-quantum = 2048;
-      # };
+      extraConfig.pipewire."context.properties" = {
+        default.clock.rate = 48000;
+        default.clock.allowed-rates = [44100 48000 96000];
+        default.clock.quantum = 1024;
+        default.clock.min-quantum = 512;
+        default.clock.max-quantum = 2048;
+      };
     };
   };
 }
