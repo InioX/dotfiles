e2df72a4e971aa4483df48910f0cc99b2fabc388
InioX <justimnix@gmail.com>
2026-07-26 22:16:43 +0200

    feat: add zed and matugen theming for zed

---
+++ nixos/modules/browsers/zen/zen.nix
@@ -0,0 +1,105 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  default,
+  ...
+}:
+with lib;
+with lib.zenyte;
+let
+  cfg = config.zenyte.browsers.zen;
+
+  extension = shortId: guid: {
+    name = guid;
+    value = {
+      install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+      installation_mode = "normal_installed";
+    };
+  };
+
+  prefs = {
+    "extensions.autoDisableScopes" = 0;
+    "extensions.pocket.enabled" = false;
+    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  };
+
+  extensions = [
+    # To add additional extensions, find it on addons.mozilla.org, find
+    # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+    # Then go to https://addons.mozilla.org/api/v5/addons/addon/!SHORT_ID!/ to get the guid
+    (extension "ublock-origin" "uBlock0@raymondhill.net")
+    # ...
+  ];
+in
+{
+  options.zenyte.browsers.zen = {
+    enable = mkBoolOpt false "Whether to enable zen.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      (pkgs.wrapFirefox
+        inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser-unwrapped
+        {
+          extraPrefs = lib.concatLines (
+            lib.mapAttrsToList (
+              name: value: "lockPref(${lib.strings.toJSON name}, ${lib.strings.toJSON value});"
+            ) prefs
+          );
+
+          extraPolicies = {
+            DisableTelemetry = true;
+            ExtensionSettings = builtins.listToAttrs extensions;
+
+            SearchEngines = {
+              Default = "ddg";
+              Add = [
+                {
+                  Name = "nixpkgs packages";
+                  URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
+                  IconURL = "https://wiki.nixos.org/favicon.ico";
+                  Alias = "@np";
+                }
+                {
+                  Name = "NixOS options";
+                  URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
+                  IconURL = "https://wiki.nixos.org/favicon.ico";
+                  Alias = "@no";
+                }
+                {
+                  Name = "NixOS Wiki";
+                  URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+                  IconURL = "https://wiki.nixos.org/favicon.ico";
+                  Alias = "@nw";
+                }
+                {
+                  Name = "noogle";
+                  URLTemplate = "https://noogle.dev/q?term={searchTerms}";
+                  IconURL = "https://noogle.dev/favicon.ico";
+                  Alias = "@ng";
+                }
+              ];
+            };
+          };
+        }
+      )
+    ];
+
+    zenyte.home.configFile = {
+      "zen/ini/chrome/" = "zen/chrome/";
+    };
+
+    zenyte.matugen.template = {
+      zen = {
+        input = "zen/browser-theme.css";
+        output = "~/.config/zen/ini/chrome/browser-theme.css";
+      };
+      zen-website-colors = {
+        input = "firefox-colors.css";
+        output = "~/.config/zen/ini/chrome/colors.css";
+      };
+    };
+  };
+}
--- nixos/modules/desktop/addons/gtk/gtk.nix
+++ nixos/modules/desktop/addons/gtk/gtk.nix
@@ -45,7 +45,6 @@ in
       "gtk-4.0/settings.ini" = "gtk-4.0/settings.ini";
       "gtk-3.0/settings.ini" = "gtk-3.0/settings.ini";
       "gtk-3.0/bookmarks" = "gtk-3.0/bookmarks";
-      "vesktop-flags.conf" = "vesktop-flags.conf";
     };
 
     zenyte.matugen.template = {
