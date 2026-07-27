386c8fe0b341ff12df26b335c7066cf5d766a631
InioX <justimnix@gmail.com>
2026-07-27 15:10:54 +0200

    feat(hyprland): add nvidia envs

--- dots/config/hypr/lua/hyprland.lua
+++ dots/config/hypr/lua/hyprland.lua
@@ -58,7 +58,10 @@ end)
 hl.env("XCURSOR_SIZE", "24")
 hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
+
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("XDG_SESSION_TYPE", "wayland")
 
 
 -----------------------
