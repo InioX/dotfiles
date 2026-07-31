cc12e4e0f2694405281c4d78c46b5541fa9be1f9
InioX <justimnix@gmail.com>
2026-07-31 18:45:31 +0200

    feat(hyprland): update cursor size

--- dots/config/hypr/lua/hyprland.lua
+++ dots/config/hypr/lua/hyprland.lua
@@ -55,9 +55,9 @@ end)
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "24")
+hl.env("XCURSOR_SIZE", "22")
 hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
-hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "22")
 
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
