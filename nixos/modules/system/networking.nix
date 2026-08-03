77b8e8ce7f0b7b090b5ce38d3d6139eb65dc0032
InioX <justimnix@gmail.com>
2026-08-03 18:30:32 +0200

    fix: use `resolved`

--- nixos/modules/system/networking.nix
+++ nixos/modules/system/networking.nix
@@ -20,15 +20,25 @@ in
     # This fixed my problem with crates.io giving bad hashes and rust packages failing to build.
     # sudo nmcli connection modify "<wifi-connection-name>" ipv4.ignore-auto-dns yes ipv4.dns "1.1.1.1,1.0.0.1,8.8.8.8,8.8.4.4"
 
+    services.resolved = {
+      enable = true;
+      dnssec = false;
+      fallbackDns = [
+        "1.1.1.1"
+        "8.8.8.8"
+      ];
+    };
+
     networking = {
       hostName = hostName;
       nameservers = [
         "1.1.1.1"
+        "1.0.0.1"
         "8.8.8.8"
       ];
       networkmanager = {
         enable = true;
-        dns = "none";
+        dns = "systemd-resolved";
         wifi = {
           powersave = false;
           # backend = "iwd";
