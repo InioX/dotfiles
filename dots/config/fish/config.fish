ec0c44d1229bf5b7131add048f049c2dff80959d
InioX <justimnix@gmail.com>
2026-08-09 22:37:44 +0200

    feat: add fish aliases

--- dots/config/fish/config.fish
+++ dots/config/fish/config.fish
@@ -6,8 +6,14 @@ starship init fish | source
 
 set fish_greeting
 
-alias ls="eza --icons=auto"
-
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    alias lst="eza --icons=auto -T --short-nix"
+    alias ls="eza --icons=auto --short-nix"
+    alias flake-update="nix flake update --flake ~/dev/dotfiles/nixos"
+    alias nix-update="nh os switch ~/dev/dotfiles/nixos"
+    alias develop="nix develop -c fish"
+
+    function nix-run
+        nix run -- "nixpkgs#$argv[1]" $argv[2..-1]
+    end
 end
