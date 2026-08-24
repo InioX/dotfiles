8ec8216e93a041508762195dfa0c7c5372d0e2bb
InioX <justimnix@gmail.com>
2026-08-24 21:12:27 +0200

    fix: use ipv4.ignore-auto-dns for network manager

--- nixos/modules/system/networking.nix
+++ nixos/modules/system/networking.nix
@@ -7,11 +7,9 @@
   ...
 }:
 with lib;
-with lib.zenyte;
-let
+with lib.zenyte; let
   cfg = config.zenyte.system.networking;
-in
-{
+in {
   options.zenyte.system.networking = {
     bluetooth = mkBoolOpt false "Whether to enable bluetooth.";
   };
@@ -20,18 +18,18 @@ in
     # This fixed my problem with crates.io giving bad hashes and rust packages failing to build.
     # sudo nmcli connection modify "<wifi-connection-name>" ipv4.ignore-auto-dns yes ipv4.dns "1.1.1.1,1.0.0.1,8.8.8.8,8.8.4.4"
 
-    services.resolved = {
-      enable = true;
-      settings = {
-        Resolve = {
-          DNSSEC = false;
-          FallbackDNS = [
-            "1.1.1.1"
-            "8.8.8.8"
-          ];
-        };
-      };
-    };
+    # services.resolved = {
+    #   enable = true;
+    #   settings = {
+    #     Resolve = {
+    #       DNSSEC = false;
+    #       FallbackDNS = [
+    #         "1.1.1.1"
+    #         "8.8.8.8"
+    #       ];
+    #     };
+    #   };
+    # };
 
     networking = {
       hostName = hostName;
@@ -42,10 +40,12 @@ in
       ];
       networkmanager = {
         enable = true;
-        dns = "systemd-resolved";
         wifi = {
           powersave = false;
           # backend = "iwd";
+        };
+        settings = {
+          ipv4.ignore-auto-dns = true;
         };
       };
 
