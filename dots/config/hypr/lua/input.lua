e2c99f45c3cef6b93f882495d761cd150b819285
InioX <justimnix@gmail.com>
2026-08-25 15:24:12 +0200

    feat(hypr): add czech keyboard

--- dots/config/hypr/lua/input.lua
+++ dots/config/hypr/lua/input.lua
@@ -1,19 +1,19 @@
 hl.config({
-    input = {
-        kb_layout  = "us,ru(phonetic_winkeys)",
-        kb_variant = "",
-        kb_options = "grp:win_space_toggle",
-        kb_model   = "",
-        kb_rules   = "",
+	input = {
+		kb_layout = "us,ru(phonetic_winkeys),cz",
+		kb_variant = "",
+		kb_options = "grp:win_space_toggle",
+		kb_model = "",
+		kb_rules = "",
 
-        follow_mouse = 1,
-        numlock_by_default = true,
+		follow_mouse = 1,
+		numlock_by_default = true,
 
-        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
-        touchpad = {
-            natural_scroll = true,
-            disable_while_typing = false,
-        },
-    },
+		touchpad = {
+			natural_scroll = true,
+			disable_while_typing = false,
+		},
+	},
 })
